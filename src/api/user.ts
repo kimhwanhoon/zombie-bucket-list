5db02f8143318428313b0860c717ce70a4b3c1af
Songju Jeong <js0ngwn@gmail.com>
2023-08-13 14:35:56 +0900

    fix: Fixed an error where user information could not be retrieved when editing. #28

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,14 +1,14 @@
-import supabase from "./supabase";
+import supabase from './supabase';
 
-export const fetchUserDB = async (email:string) => {
+export const fetchUserDB = async (email: string) => {
   // 현재 유저의 프로필URL과 nickname 가져오기
-  console.log(" >>>>>", email)
+  console.log(' >>>>>', email);
   const { data, error } = await supabase
     .from('users')
-    .select('nickname, profileImage')
+    .select('nickname, profileImage, about')
     .eq('email', email);
 
-  console.log("더이상..쓸말이..", data)
+  console.log('더이상..쓸말이..', data);
 
   if (error) {
     alert(
